3593b34d908373bb162c3db31ef9840c6889f67c
vagrant <vagrant@precise64.(none)>
2014-01-04 21:29:34 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DavinciShoppingCart::Application.config.secret_key_base = '9e4a1425124c5f046b0c9beba1809de6b05939f9171f8a50c235647467bf9cebbbcb530830ef32bd86fbb678013e6961b3056b5639dfb1cfe92c92f89bc70f26'
